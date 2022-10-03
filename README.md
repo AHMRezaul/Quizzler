0037bea84518c7d9dec91a5907362709c8034fc6
A. H. M. Rezaul Karim <rezaulkarim@iut-dhaka.edu>
2022-10-03 20:35:25 +0600

    Updates

---
+++ README.md
@@ -0,0 +1,48 @@
+<h1 align="center">
+<img src="https://github.com/AHMRezaul/Quizzler/blob/main/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.png"  width="200" height="200" />
+<br> Quizzler </br>
+</h1>
+
+<h4 align="center">
+A mobile application developed using flutter technologies. 
+APK installation link: https://github.com/AHMRezaul/Quizzler/blob/main/quizzler/universal.apk
+</h4>
+
+## Overview
+A mobile application created using flutter app development technologies. This application can be run on any platforms (android or iOS).
+It is a quiz app that takes the input as **True** or **False** from the user and keeps track of the score. At the end of the quiz, it displays an alert to let the user know that the quiz is finished and resets the quiz.
+## Tools:
+The application is developed using **Flutter Technologies** and **Dart** language. Flutter is a technology developed by Google that can create both an **Android** and an **iOS** application simultaneously.
+The app uses the *Stateful Widget* tool of the Dart language in order to update the states of the app without needing to restart the app. 
+The following tools and software were used to develop the application:
+1. Flutter
+2. Dart
+3. Android Studio
+4. Emulator
+
+### Technical details:
+Flutter version: 2.8.1
+
+Dart SDK version: 2.1.5
+
+rflutter_alert (Dart package): 1.0.2
+
+## Installation:
+To run the project, clone the repository and open it using **Android Studio**.
+Open the **pubspec.yaml** file and run the following command.
+```
+flutter pub get
+```
+This will download all the dependencies and packages required for the project. 
+Next, run the emulator and finally build and run the application on the emulator.
+
+In order to change the questions, edit the **quiz_brain.dart** file.
+
+To install the application, download from the link: https://github.com/AHMRezaul/Quizzler/blob/main/quizzler/universal.apk
+
+## App Preview:
+During quiz:
+![screenshot](https://github.com/AHMRezaul/Quizzler/blob/main/quiz-ss.png)
+End of the quiz alert:
+![screenshot](https://github.com/AHMRezaul/Quizzler/blob/main/alert-ss.png)
+
---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getQuestionAnswer();
+
+    setState(() {
+      //check if quiz finished
+      if (quizBrain.isFinished() == true) {
+        //create alert
+        Alert(
+          context: context,
+          title: 'Finished',
+          desc: 'You have reached the end of the quiz.',
+        ).show();
+//reset quiz
+        quizBrain.reset();
+//reset score
+        scoreKeeper = [];
+      } else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        //Question text
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        //True choice
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        //False Choice
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
